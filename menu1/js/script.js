28825582213292161ae875bf8690fa9f24e06624
Shubham Sutar <65288339+shubhamstr@users.noreply.github.com>
2020-11-01 20:38:31 +0530

    Menubar created responsive
    for practice

---
+++ menu1/js/script.js
@@ -0,0 +1,10 @@
+let openNav = () => {
+    document.getElementById('menu').style.display="block";
+    document.getElementById('menubtn').style.display="none";
+    document.getElementById('closebtn').style.display="block";
+} 
+let closeNav = () => {
+    document.getElementById('menu').style.display="none";
+    document.getElementById('menubtn').style.display="block";
+    document.getElementById('closebtn').style.display="none";
+} 
